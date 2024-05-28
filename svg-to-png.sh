03883b5154c7ff1f1e82b4387a9d43e290710067
joergnapp <jn@joergnapp.de>
2024-05-28 20:10:45 +0200

    Create output directory

--- svg-to-png.sh
+++ svg-to-png.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# see https://gist.github.com/mohanpedala/1e2ff5661761d3abd0385e8223e16425
 set -euxo pipefail
 IFS=$'\n\t'
 
@@ -7,6 +8,10 @@ SVGPATH=${1:-"svg/"}
 PNGPATH=${2:-"png/"}
 ZOOM=${3:-1}
 
+# create directory to store pngs
+mkdir -p "$PNGPATH"
+
+# search fpr SVGs
 FILES=$(find "${SVGPATH}" -maxdepth 1 -type f -iname '*.svg')
 
 echo "$FILES" | while read -r INFILE
